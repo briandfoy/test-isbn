9c577a11a6b958c01b97a2ab94b1275f95f77e48
brian d foy <brian.d.foy@gmail.com>
2004-09-08 09:10:23 +0000

    * cleaned up distro
    * bumped version to major release
    
    * added README and META.yml
    
    
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@1470 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/ISBN.pm
+++ lib/ISBN.pm
@@ -11,7 +11,7 @@ use Test::Builder;
 
 my $Test = Test::Builder->new();
 
-$VERSION = '0.5_04';
+$VERSION = sprintf "%d.%02d", q$Revision$ =~ m/ (\d+) \. (\d+) /xg;
 @EXPORT  = qw(isbn_ok isbn_country_ok isbn_publisher_ok);
 
 =head1 NAME
@@ -22,12 +22,12 @@ Test::ISBN - Check International Standard Book Numbers
 
 	use Test::More tests => 1;
 	use Test::ISBN;
-	
+
 	isbn_ok( $isbn );
-	
+
 =head1 DESCRIPTION
 
-=head1 FUNCTIONS
+=head2 Functions
 
 =over 4
 
@@ -41,16 +41,16 @@ be invalid even though the checksum is valid.
 =cut
 
 sub isbn_ok
-	{	
+	{
 	my $isbn = shift;
-	my $ok   = Business::ISBN::is_valid_checksum( $isbn ) 
+	my $ok   = Business::ISBN::is_valid_checksum( $isbn )
 		eq Business::ISBN::GOOD_ISBN;
-	
+
 	$Test->ok( $ok );
 
 	$Test->diag( "The string [$isbn] is not a valid ISBN" ) unless $ok;
 	}
-	
+
 =item isbn_country_ok( STRING, COUNTRY )
 
 Ok is the STRING is a valid ISBN and its country
@@ -63,7 +63,7 @@ sub isbn_country_ok
 	my $isbn    = shift;
 	my $country = shift;
 	my $object  = Business::ISBN->new($isbn);
-	
+
 	unless( $object->is_valid )
 		{
 		$Test->ok(0);
@@ -80,7 +80,7 @@ sub isbn_country_ok
 			"\tExpected [$country]\n",
 			"\tGot [" . $object->country_code . "]\n" );
 		}
-		
+
 	}
 
 =item isbn_publisher_ok( STRING, PUBLISHER )
@@ -89,13 +89,13 @@ Ok is the STRING is a valid ISBN and its publisher
 code is the same as PUBLISHER.
 
 =cut
-	
+
 sub isbn_publisher_ok
 	{
 	my $isbn      = shift;
 	my $publisher = shift;
 	my $object    = Business::ISBN->new($isbn);
-	
+
 	unless( $object->is_valid )
 		{
 		$Test->ok(0);
@@ -113,16 +113,26 @@ sub isbn_publisher_ok
 			"\tGot [" . $object->publisher_code . "]\n" );
 		}
 	}
-	
+
 =back
+
+=head1 SOURCE AVAILABILITY
+
+This source is part of a SourceForge project which always has the
+latest sources in CVS, as well as all of the previous releases.
+
+	http://sourceforge.net/projects/brian-d-foy/
+
+If, for some reason, I disappear from the world, one of the other
+members of the project can shepherd this module appropriately.
 
 =head1 AUTHOR
 
-brian d foy, E<lt>brian d foyE<gt>
+brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT
 
-Copyright 2002 brian d foy, All rights reserved.
+Copyright 2002-2004 brian d foy, All rights reserved.
 
 You can use this module under the same terms as
 Perl itself.
--- t/load.t
+++ t/load.t
@@ -1,17 +1,11 @@
 # $Id$
 BEGIN {
-	use File::Find::Rule;
-	@classes = map { my $x = $_;
-		$x =~ s|^blib/lib/||;
-		$x =~ s|/|::|g;
-		$x =~ s|\.pm$||;
-		$x;
-		} File::Find::Rule->file()->name( '*.pm' )->in( 'blib/lib' );
+	@classes = qw(Test::ISBN);
 	}
 
 use Test::More tests => scalar @classes;
-	
+
 foreach my $class ( @classes )
 	{
-	use_ok( $class );
+	print "bail out! $class did not compile\n" unless use_ok( $class );
 	}
